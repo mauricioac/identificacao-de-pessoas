4a84835f90c2764c7ac80afd58514886d09b9a9e
Maurício André Cinelli <mauricio.andre.cinelli@gmail.com>
2016-04-11 17:41:41 -0300

    Renomeando variáveis e adicionando readme

---
+++ README.txt
@@ -0,0 +1,17 @@
+Trabalho de detecção de pessoas
+
+Alunos: Silvana Trindade e Maurício André Cinelli
+
+Importante: Código feito para ambiente Linux, testado em Debian 7.9
+
+## Instrução para compilar
+
+Baixar vídeo do projeto Caviar citado na descrição do trabalho e colar na pasta junto com os códigos com o nome original "OneStopMoveEnter2cor.mpg".
+
+Para compilar, basta executar:
+
+make
+
+e para executar o programa:
+
+./video
--- video.cpp
+++ video.cpp
@@ -25,24 +25,24 @@ int  main()
 	}
 
 	/**
-	 * Corta a parte superior do frame, para
+	 * Corta a parte superior do quadro, para
 	 * eliminar a parte da informação de tempo
 	 * da câmera
 	 */
-	Mat acc, frame, temp;
+	Mat media_corrida, quadro, temp;
 	Rect ROI(0,10, 384, 278);
-	frame = imread("fundo.png",CV_LOAD_IMAGE_COLOR);//lê plano de fundo inicial
-	frame = frame(ROI);
+	quadro = imread("fundo.png",CV_LOAD_IMAGE_COLOR);//lê plano de fundo inicial
+	quadro = quadro(ROI);
 
-	acc = Mat::zeros(frame.size(), CV_32FC3);  // preenche com 0 (zero) a imagem do acumulador
-	
-    accumulateWeighted(frame, acc, 1);//pega imagem de fundo e aplica como acumulado
+	media_corrida = Mat::zeros(quadro.size(), CV_32FC3);  // preenche com 0 (zero) a imagem do acumulador
 
-    //elemento estruturante com a matriz de 3X7  
-    Mat element = getStructuringElement(MORPH_RECT, Size(3, 7)); 
+    accumulateWeighted(quadro, media_corrida, 1);//pega imagem de fundo e aplica como acumulado
+
+    //elemento estruturante com a matriz de 3X7
+    Mat elemento = getStructuringElement(MORPH_RECT, Size(3, 7));
 
     /**
-     * Percorre todo o video, para 
+     * Percorre todo o video, para
      * encontrar um background melhor
      */
     cout << "Processando vídeo." << endl;
@@ -51,15 +51,12 @@ int  main()
         cap >> temp;
         if ( temp.empty() ) break;
 
-        frame = temp(ROI);
+        quadro = temp(ROI);
 
-        Mat floatimg;
-        frame.convertTo(floatimg, CV_32FC3);
-
-        accumulateWeighted(frame, acc, 0.001);//aplica média corrida
+        accumulateWeighted(quadro, media_corrida, 0.001);//aplica média corrida
     }
     cout << "Iniciando reconhecimento de pessoas." << endl;
-   
+
     namedWindow("Video",0);//abre uma janela com o nome "Video"
     namedWindow("Diferenca",0);
 
@@ -69,51 +66,51 @@ int  main()
 
 	while(1)
 	{
-		cap2 >> temp;//pega o frame
+		cap2 >> temp;//pega o quadro
 		if ( temp.empty() ) break;
 
-		frame = temp(ROI);//corta a informação contendo data e hora
+		quadro = temp(ROI);//corta a informação contendo data e hora
 
-        accumulateWeighted(frame, acc, 0.0001);
-        Mat res;//plano de fundo
-        convertScaleAbs(acc, res);
+        accumulateWeighted(quadro, media_corrida, 0.0001);
+        Mat fundo;//plano de fundo
+        convertScaleAbs(media_corrida, fundo);
 
         Mat diff;
-        subtract(res, frame, diff);//subtrai o plano de fundo do frame atual
+        subtract(fundo, quadro, diff);//subtrai o plano de fundo do quadro atual
 
-        vector<vector<Point> > contours;
-        cv::Mat thresholded;
-        cv::cvtColor(diff, thresholded, CV_RGB2GRAY);//converte frame para escala de cinza
-        
+        vector<vector<Point> > contornos;
+        cv::Mat imagem_binaria;
+        cv::cvtColor(diff, imagem_binaria, CV_RGB2GRAY);//converte quadro para escala de cinza
+
         //Realiza duas operações de fechamento (dilatação+erosão)
-        morphologyEx(thresholded, thresholded, CV_MOP_CLOSE, element);
-        morphologyEx(thresholded, thresholded, CV_MOP_CLOSE, element);
-        
+        morphologyEx(imagem_binaria, imagem_binaria, CV_MOP_CLOSE, elemento);
+        morphologyEx(imagem_binaria, imagem_binaria, CV_MOP_CLOSE, elemento);
+
         //Aplica um limiar, se a intensidade do pixel passar de 50 é branco
-        cv::threshold(thresholded, thresholded, 50, 255, CV_THRESH_BINARY);
-        Mat t = thresholded.clone();//faz uma cópia
+        cv::threshold(imagem_binaria, imagem_binaria, 50, 255, CV_THRESH_BINARY);
+        Mat t = imagem_binaria.clone();//faz uma cópia para mostrar depois
 
         //Encontra contornos na imagem
         //Internamente utiliza crescimento de regiões
         //Retorna somente os retângulos mais externos
-        findContours(thresholded, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+        findContours(imagem_binaria, contornos, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
 
-        for (int i = 0; i < (int) contours.size(); i++) 
+        for (int i = 0; i < (int) contornos.size(); i++)
         {
             //pega o retângulo que engloba o contorno
-            Rect rect  = boundingRect(contours[i]);
+            Rect rect  = boundingRect(contornos[i]);
 
             if (rect.area() > 30)
             {
-        	   rectangle(frame, rect, Scalar(255, 0, 0));
+        	   rectangle(quadro, rect, Scalar(255, 0, 0));
             }
         }
 
-        imshow("Video",frame);//exibe o video 
+        imshow("Video",quadro);//exibe o video
         imshow("Diferenca",t);//exibe diferença
-        
+
         //espera comando de saída "ESC"
-        if (waitKey(30) == 27) 
+        if (waitKey(30) == 27)
         {
         	break;
         }
